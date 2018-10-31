39863f5c8c0dd82295ed9d83a3664c6fee5cd023
Boubacar Sow <boubacar.sow@uha.fr>
2018-10-31 11:32:07 +0100

    creation of the DAO of media and mediaType

---
+++ src/main/java/fr/miage/core/repository/MediaTypeRepository.java
@@ -0,0 +1,9 @@
+package fr.miage.core.repository;
+
+import fr.miage.core.entity.MediaType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaTypeRepository extends JpaRepository<MediaType, Long> {
+
+    MediaType findByName( String name);
+}
