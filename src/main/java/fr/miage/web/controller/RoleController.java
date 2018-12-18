ac40004d38155229647160e6d79605317d3f5895
aziraradil <adil.azirar@uha.fr>
2018-12-18 11:01:49 +0100

    View,ajout,suppr,mod,list (role)

--- src/main/java/fr/miage/web/controller/RoleController.java
+++ src/main/java/fr/miage/web/controller/RoleController.java
@@ -2,6 +2,8 @@ package fr.miage.web.controller;
 
 import fr.miage.core.entity.Role;
 import fr.miage.core.service.RoleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,46 +21,52 @@ public class RoleController {
     private RoleService roleService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String findAll(Model model) {
-        final List<Role> role = this.roleService.findAll();
-        model.addAttribute("role", role);
-        return "role/list";
-    }
-
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String create(Model model) {
-        model.addAttribute("role", new Role());
-        return "role/form";
+    public String index(Model model) {
+        /***********  List des roles   *****************/
+        model.addAttribute("roles", this.roleService.findAll());
+        /***********  Ajout d'un role ****************/
+        model.addAttribute("action","/role/create");
+        model.addAttribute("Role", new Role());
+        model.addAttribute("roles", roleService.findAll());
+        /*************   Title and Content html*******************************/
+        model.addAttribute("title", "Roles");
+        model.addAttribute("content", "role/index");
+        model.addAttribute("urlRole","roles");
+        return "base";
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String submitCreate(@Valid @ModelAttribute Role role, BindingResult bindingResult, Model model) {
-
+        //LOGGER.info("******* create User *******");
         if (bindingResult.hasErrors()) {
-            return "role/form";
+            /***********  errors user create ****************/
+            //LOGGER.info("-----------> errors user create");
+            model.addAttribute("action","/role/create");
+            model.addAttribute("Role", role);
+            /*************   Title and Content html*******************************/
+            model.addAttribute("title", "Roles");
+            model.addAttribute("content", "role/index");
+            return "base";
         }
-
+        // TO DO
+        // Encrypt password
+        // user.setUserPassword();
         roleService.save(role);
         return "redirect:/role";
     }
-
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
     public String edit(@RequestParam("id") Long id, Model model) {
-        model.addAttribute("role", this.roleService.findById(id));
-        return "role/form";
+        model.addAttribute("Role", roleService.findById(id));
+        model.addAttribute("roles", roleService.findAll());
+        String action="/user/create";
+        model.addAttribute("action",action);
+        /*************   Title and Content html*******************************/
+        String title="Modification";
+        model.addAttribute("title", title);
+        String content="user/index";
+        model.addAttribute("content", content);
+        return "base";
     }
-
-    @RequestMapping(value = "/edit", method = RequestMethod.POST)
-    public String submitEdit(@Valid @ModelAttribute Role role, BindingResult bindingResult, Model model) {
-
-        if (bindingResult.hasErrors()) {
-            return "role/form";
-        }
-
-        roleService.save(role);
-        return "redirect:/role";
-    }
-
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String delete(@PathVariable("id") Long id) {
         roleService.delete(id);
