b92274e0561257fd89993f455b34be835a607cac
ELASLAOUI <nabil.elaslaoui@uha.fr>
2019-01-09 00:42:07 +0100

    update Password Encoder

--- src/main/java/fr/miage/core/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.miage.core.repository;
-
-import fr.miage.core.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByName(String name);
-}
