f7edd002ad2fe053523f79430816263830fd0a1f
aziraradil <adil.azirar@uha.fr>
2019-01-17 14:39:32 +0100

    Add SubscriptionTypeRepository.java

--- src/main/java/fr/miage/core/repository/SubscriptionTypeRepository.java
+++ src/main/java/fr/miage/core/repository/SubscriptionTypeRepository.java
@@ -4,7 +4,7 @@ import fr.miage.core.entity.MediaType;
 import fr.miage.core.entity.SubscriptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType, Long> {
+public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType , Long> {
 
     SubscriptionType findByTypeName(String name);
 
